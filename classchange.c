07a4398f263f5f3425b8b1f56a5daf409f2448a4
carterprince <carteraprince@gmail.com>
2023-04-10 20:09:58 -0400

    add classes

--- classchange.c
+++ classchange.c
@@ -12,6 +12,8 @@ int mon_schedule[6][3] = {
     {15, 50, 00},
 };
 
+char *mon_classes[] = {"MATH", "MATH", "POLS", "POLS", "LING", "LING"};
+
 int tue_thu_schedule[8][3] = {
     {9, 35, 00},
     {10, 50, 00},
@@ -23,12 +25,16 @@ int tue_thu_schedule[8][3] = {
     {17, 10, 00},
 };
 
+char *tue_thu_classes[] = {"MATH", "MATH", "CSCI", "CSCI", "POLS", "POLS", "CSCI", "CSCI"};
+
 int wed_fri_schedule[2][3] = {
     {15, 00, 00},
     {15, 50, 00},
 };
 
-long get_seconds_left(int schedule[][3], int schedule_length, time_t ts) {
+char *wed_fri_classes[] = {"LING", "LING"};
+
+long get_seconds_left(int schedule[][3], int schedule_length, time_t ts, int *class_index) {
     struct tm *tm;
     long int seconds_left = 0;
     for (int i = 0; i < schedule_length; i++) {
@@ -39,8 +45,10 @@ long get_seconds_left(int schedule[][3], int schedule_length, time_t ts) {
 
             seconds_left = mktime(tm) - ts;
 
-            if (seconds_left > 0)
+            if (seconds_left > 0) {
+                *class_index = i;
                 break;
+            }
         }
     }
 
@@ -61,21 +69,27 @@ int main() {
     struct tm *now = localtime(&ts);
 
     long seconds_left = -1;
+    int class_index = -1;
+    char **classes = NULL;
+
     switch (now->tm_wday) {
         case 1: // Monday
-            seconds_left = get_seconds_left(mon_schedule, 6, ts);
+            seconds_left = get_seconds_left(mon_schedule, 6, ts, &class_index);
+            classes = mon_classes;
             break;
         case 2: // Tuesday
         case 4: // Thursday
-            seconds_left = get_seconds_left(tue_thu_schedule, 8, ts);
+            seconds_left = get_seconds_left(tue_thu_schedule, 8, ts, &class_index);
+            classes = tue_thu_classes;
             break;
         case 3: // Wednesday
         case 5: // Friday
-            seconds_left = get_seconds_left(wed_fri_schedule, 2, ts);
+            seconds_left = get_seconds_left(wed_fri_schedule, 2, ts, &class_index);
+            classes = wed_fri_classes;
             break;
     }
 
-    if (seconds_left != -1) {
+    if (seconds_left != -1 && class_index != -1) {
         if (seconds_left == 30)
             send_notification("pack up", "30 seconds until class change");
 
@@ -88,7 +102,7 @@ int main() {
         if (minute > 59) {
             minute = minute - (60 * hour);
         }
-        printf("%02d:%02d:%02d\n", hour, minute, seconds);
+        printf("%s (%02d:%02d:%02d)\n", classes[class_index], hour, minute, seconds);
     }
 
     return 0;
